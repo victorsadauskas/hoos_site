c2cb71d45a60a9e16d23c18d927c14ed07eb3ac3
Victor Sadauskas <victor.sadauskas@gmail.com>
2017-03-05 21:13:17 -0600

    Added files and changes required for deployment to Heroku

--- hoossite/urls.py
+++ hoossite/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
 from home.views import IndexView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('home.urls')),
-    url(r'^$', IndexView.as_view())
+    url(r'^$', RedirectView.as_view(url='/home/', permanent=True)),
 ]
+
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
